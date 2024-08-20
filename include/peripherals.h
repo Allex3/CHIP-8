7b96dbe376a9aeb213ed2d97cbd19a9561e8606d
Allex3 <alex.iordachioaiei3@gmail.com>
2024-08-20 05:02:59 +0300

    Add functions to peripherals.h header

---
+++ include/peripherals.h
@@ -0,0 +1,9 @@
+#ifndef PERIPHERALS_H_
+#define PERIPHERALS_H_
+
+void initDisplay();
+void draw(unsigned char* display); //draw pixels using the display array
+void sdlEventHandler(unsigned char* keypad); //use SDL events for keypad
+void stopDisplay();
+
+#endif
